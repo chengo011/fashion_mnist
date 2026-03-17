fa44ce3a84b86522b930a9f6cf69c7288e1b3aec
Chyngyz <chyngyzk05@icloud.com>
2026-03-17 17:56:18 +0100

    created base

---
+++ config.py
@@ -0,0 +1,23 @@
+CLASS_NAMES = [
+    "T-Shirt/Top",  # 0
+    "Trouser",       # 1
+    "Pullover",      # 2
+    "Dress",         # 3
+    "Coat",          # 4
+    "Sandal",        # 5
+    "Shirt",         # 6
+    "Sneaker",       # 7
+    "Bag",           # 8
+    "Ankle Boot",    # 9
+]
+
+NUM_CLASSES = 10
+IMG_HEIGHT = 28
+IMG_WIDTH = 28
+IMG_SIZE = IMG_HEIGHT * IMG_WIDTH
+
+PLOTS_DIR = "plots"
+MODELS_DIR = "models"
+DATA_DIR = "data"
+
+RANDOM_STATE = 42
---
+++ data_loader.py
@@ -0,0 +1,30 @@
+import numpy as np
+from tensorflow.keras.datasets import fashion_mnist
+from config import CLASS_NAMES
+
+def load_data():
+    return (X_train, y_train), (X_test, y_test)
+
+def normalize(X_train, X_test):
+    X_train = X_train.astype(np.float32) / 255.0
+    X_test = X_test.astype(np.float32) / 255.0
+    return X_train, X_test
+
+def flatten(X_train, X_test):
+    X_train_flat = X_train.reshape(X_train.shape[0], -1)
+    X_test_flat = X_test.reshape(X_test.shape[0], -1)
+    return X_train_flat, X_test_flat
+
+def prepare_for_ml(X_train, X_test):
+    X_train_norm, X_test_norm = normalize(X_train, X_test)
+    X_train_flat, X_test_flat = flatten(X_train_norm, X_test_norm)
+    return X_train_flat, X_test_flat
+
+def prepare_for_cnn(X_train, X_test):
+    X_train_norm, X_test_norm = normalize(X_train, X_test)
+
+    X_train_cnn = X_train_norm[..., np.newaxis]
+    X_test_cnn = X_test_norm[..., np.newaxis]
+
+    return X_train_cnn, X_test_cnn
+
